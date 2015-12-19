55dc4c335e2188a3792db7546be276635118be48
vrachimis <avrachimi@hotmail.com>
2015-12-19 17:42:06 +0200

    smems

--- GreenPlay_Unity/Assets/Vrachimis/Scripts/OxygenGravity.cs
+++ GreenPlay_Unity/Assets/Vrachimis/Scripts/OxygenGravity.cs
@@ -6,6 +6,8 @@ public class OxygenGravity : MonoBehaviour {
 	public GameObject gravityObject;
 	public float acceleration = 9.81f;
 
+	private Transform lastPosition;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -17,5 +19,12 @@ public class OxygenGravity : MonoBehaviour {
 		//GetComponent<Rigidbody2D>().AddForce(new Vector2(0,0.5f));
 		GetComponent<Rigidbody2D>().gravityScale = -acceleration;
 		//GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+	}
+
+	void FixedUpdate() {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+		if (hit.collider != null) {
+			//adsf
+		}
 	}
 }
