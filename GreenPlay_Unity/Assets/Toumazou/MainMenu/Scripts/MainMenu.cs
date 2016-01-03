33f72e0764cdf1bbfe3f8f66e04d33def7f04702
Toumazou <yiannistoumazou@gmail.com>
2016-01-03 17:29:25 +0200

    Main menu 2
    Changes in design

--- GreenPlay_Unity/Assets/Toumazou/MainMenu/Scripts/MainMenu.cs
+++ GreenPlay_Unity/Assets/Toumazou/MainMenu/Scripts/MainMenu.cs
@@ -11,17 +11,26 @@ public class MainMenu : MonoBehaviour {
 	public Texture background;
 	public GUIStyle PlayButtonTexture;//image of play button
 	public GUIStyle OptionsButtonTexture;//image of options button
+	public GUIStyle LeaderboardButtonTexture;//image of leaderboard button
+	public GUIStyle ShopButtonTexture;//image of Shop button
 	public float PlayButtonPlacementY; // y position of the button play
 	public float OptionsButtonPlacementY; // y position of the button options
+
 	public float PlayButtonPlacementX; // x position of the button play
 	public float OptionsButtonPlacementX; // x position of the button options
+
+	public float LeaderboardButtonPlacementY; // y position of the button Leaderboard
+	public float LeaderboardButtonPlacementX; // x position of the button Leaderboard
+
+	public float ShopButtonPlacementY; // y position of the button Shop
+	public float ShopButtonPlacementX; // x position of the button Shop
 
 	void OnGUI(){
 	//Display our background texture
 		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), background); 
 
 	//Display our buttons with textures
-		if (GUI.Button (new Rect(Screen.width * PlayButtonPlacementX, Screen.height * PlayButtonPlacementY,Screen.width * .5f, Screen.height * .1f) ,"", PlayButtonTexture)){
+		if (GUI.Button (new Rect(Screen.width * PlayButtonPlacementX, Screen.height * PlayButtonPlacementY,Screen.width * .6f, Screen.height * .2f) ,"", PlayButtonTexture)){
 			print ("Clicked");
 		}
 
@@ -29,6 +38,13 @@ public class MainMenu : MonoBehaviour {
 			print ("Clicked");
 		}
 
+		if (GUI.Button (new Rect(Screen.width * LeaderboardButtonPlacementX, Screen.height * LeaderboardButtonPlacementY,Screen.width * .24f, Screen.height * .10125f) ,"", LeaderboardButtonTexture)){
+			print ("Clicked");
+		}
+
+		if (GUI.Button (new Rect(Screen.width * ShopButtonPlacementX, Screen.height * ShopButtonPlacementY,Screen.width * .24f, Screen.height * .10125f) ,"", ShopButtonTexture)){
+			print ("Clicked");
+		}
 
 	}
 
